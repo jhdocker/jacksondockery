7591cfe6778d32c16267cb1553664fec411ddeaa
Jackson Dockery <jhdocker@gmail.com>
2015-04-22 16:36:04 -0400

    completing form handlin

--- form.php
+++ form.php
@@ -23,7 +23,7 @@ require 'phpmailer/PHPMailerAutoload.php';
 
 $mail = new PHPMailer;
 
-//$mail->SMTPDebug = 1;                               // Enable verbose debug output
+$mail->SMTPDebug = 1;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
